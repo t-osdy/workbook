811e9118546ffebaf187f69a82971908e7d53b8e
tosada <tomoya.a.osada@gmail.com>
2016-03-21 22:55:03 +0900

    add leap year

---
+++ ruby/leap_year.rb
@@ -0,0 +1,29 @@
+def leap_year(num)
+  leap_flag = false
+
+  if num % 400 == 0
+    leap_flag = true
+    display_print(num, leap_flag)
+  elsif num % 100 == 0
+    leap_flag = false
+    display_print(num, leap_flag)
+  elsif num % 4 == 0
+    leap_flag = true
+    display_print(num, leap_flag)
+  else
+    leap_flag = false
+    display_print(num, leap_flag)
+  end
+end
+
+def display_print(num, leap_flag)
+  if leap_flag == true
+    puts "#{num}はうるう年です"
+  else
+    puts "#{num}はうるう年ではありません"
+  end
+end
+
+puts "西暦を入力してください"
+num = gets.chomp.to_i
+leap_year(num)
